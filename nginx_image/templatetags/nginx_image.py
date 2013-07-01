494e6164b399aa72fbe2790aa4c71184f33aaab2
Euan Lau <euanlau@gmail.com>
2013-07-01 10:37:03 +0800

    Handle image_url is none

--- nginx_image/templatetags/nginx_image.py
+++ nginx_image/templatetags/nginx_image.py
@@ -20,11 +20,15 @@ def thumbnail(image_url_or_fieldfile, width=None, height=None, crop=False):
 
     params = urllib.urlencode(params_dict)
 
+    image_url = None
     if isinstance(image_url_or_fieldfile, (ImageFieldFile, )):
         if getattr(image_url_or_fieldfile, 'name', None) and hasattr(image_url_or_fieldfile, 'url'):
             image_url = image_url_or_fieldfile.url
     else:
         image_url = image_url_or_fieldfile
+
+    if not image_url:
+        return None
 
     url = "{image_url}?{params}".format(
         image_url=image_url,
